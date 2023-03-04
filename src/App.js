a17e68e26c9269be6437ce2d4933b1415fba6684
Dragonkeun <dragonkeun7@gmail.com>
2023-03-04 09:55:06 +0900

    쇼핑몰 Product 컴포넌트로 상품 배치 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import data from "./data.js";
+import { useState } from "react";
 function App() {
+  let [shoes, setShoes] = useState(data);
   return (
     <div className="App">
-      react-shop
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="main-bg"></div>
+      <div className="container">
+        <div className="row">
+          {shoes.map((a, i) => {
+            return <Product shoes={shoes[i]} i={i + 1} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Product(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"}
+        alt="상품 사진"
+        width="80%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
     </div>
   );
 }
