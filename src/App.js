450764def277b51e44dcd6e96e27ce45739b1982
Dragonkeun <dragonkeun7@gmail.com>
2023-03-04 12:41:08 +0900

    라우터 공부, nested routes 구현

--- src/App.js
+++ src/App.js
@@ -2,27 +2,59 @@ import "./App.css";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import data from "./data.js";
 import { useState } from "react";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import Detail from "./routes/Detail.js";
 function App() {
   let [shoes, setShoes] = useState(data);
+  let navigate = useNavigate();
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">shop</Navbar.Brand>
+          <Navbar.Brand
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            shop
+          </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/detail");
+              }}
+            >
+              DETAIL
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className="main-bg"></div>
-      <div className="container">
-        <div className="row">
-          {shoes.map((a, i) => {
-            return <Product shoes={shoes[i]} i={i + 1} />;
-          })}
-        </div>
-      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <div className="container">
+                <div className="row">
+                  {shoes.map((a, i) => {
+                    return <Product shoes={shoes[i]} i={i + 1} />;
+                  })}
+                </div>
+              </div>
+            </>
+          }
+        />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/about" element={<About />}>
+          <Route
+            path="member"
+            // 이건 nested routes인데 /about/member로 접속할 때 /about에 화면을 가져가면서 페이지 구성하고 싶을 때 사용, 그냥 /about으로 접속 시 About 컴포넌트 보여주고, /about/member 접속 시 About 컴포넌트 + /about/member로 접속하셨습니다 라는 div까지 보여줌
+            element={<div>/about/member로 접속하셨습니다</div>}
+          />
+        </Route>
+        <Route path="*" element={<div>없는 페이지</div>} />
+      </Routes>
     </div>
   );
 }
@@ -41,4 +73,13 @@ function Product(props) {
   );
 }
 
+function About() {
+  return (
+    <div>
+      <h4>회사 정보</h4>
+      {/* Outlet을 작성한 자리에 member element를 보여줌 */}
+      <Outlet/>
+    </div>
+  );
+}
 export default App;
